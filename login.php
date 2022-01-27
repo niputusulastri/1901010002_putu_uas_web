38cce4b4829d6010297a30c31d5e77a2ac5ed0e1
niputusulastri <92281343+niputusulastri@users.noreply.github.com>
2022-01-27 23:25:54 +0800

    Update login.php

--- login.php
+++ login.php
@@ -26,7 +26,7 @@ if (isset($_GET['login'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Lombok Tour Travel</title>
+    <title>Perpustakaan Universitas Bumigora</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="https://parsleyjs.org/src/parsley.css">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
